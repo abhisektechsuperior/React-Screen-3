ffc653078114bbf9198cfe5fc9e5116baacca7f7
Abhisek Kumar <140617172+abhisektechsuperior@users.noreply.github.com>
2023-09-26 11:58:57 +0530

    Add files via upload

---
+++ src/Components/IconComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import twitter from '../assets/twitt black icon.png'
+
+function IconComponent() {
+    const openURL = () => {
+        const urlToOpen = 'https://twitter.com/?logout=1695707002873'; // Replace with the URL you want to open
+        window.open(urlToOpen, '_blank'); // '_blank' opens the URL in a new tab or window
+    };
+
+    return (
+        <div>
+
+            {/* Add your icon component here */}
+            <img className='footerimage' src={twitter} onClick={openURL} alt="" />
+
+        </div>
+    );
+}
+
+export default IconComponent;
